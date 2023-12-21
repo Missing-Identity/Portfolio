c1a82b2d59644693df462961aaaa6510fb1221aa
Unmilan <145965499+unmilanm@users.noreply.github.com>
2023-12-21 21:04:37 +0530

    Database Creation

--- main.py
+++ main.py
@@ -133,4 +133,6 @@ def delete_project(project_id):
     return redirect(url_for('index'))
 
 if __name__ == "__main__":
+    with app.app_context():
+        db.create_all()
     app.run(debug=False)
